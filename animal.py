849d3d41a3f3145d342438f8ee5ea3852b3f730a
carolpang <carolpang@tencent.com>
2023-02-05 09:17:43 +0800

    feat: add Dog function

--- animal.py
+++ animal.py
@@ -2,9 +2,15 @@ import sys
 
 def default():
     print("hello")
+
+def dog():
+    print("Woof")
     
 def main():
-    default()
+    if sys.argv[1] == 'dog':
+        dog()
+    else:
+        default()
     
 if __name__ == '__main__':
     main()
