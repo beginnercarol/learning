71c2266bb7be2aec3c722d2ca6fb44d68a22dc8f
carolpang <carolpang@tencent.com>
2023-02-05 09:14:03 +0800

    feat: add Cat function

--- animal.py
+++ animal.py
@@ -3,8 +3,16 @@ import sys
 def default():
     print("hello")
     
+def cat():
+    print("Kitty says hello")
+    
 def main():
-    default()
+    if len(sys.argv) < 2:
+        default()
+    elif sys.argv[1] == 'cat':
+        cat()
+    else:
+        default()
     
 if __name__ == '__main__':
     main()
